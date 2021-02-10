4331710c50c0e07e8351ca2ecff84fc7c3d05152
Marcelo Ian <marcelo.menezes@estudante.ufjf.br>
2021-02-10 11:16:17 -0300

    Revert "cria balde.hpp e refatora"
    This reverts commit 98e4a7619566e4b07efc04c274fc58ac85661580.

--- Balde.cpp
+++ Balde.cpp
@@ -1,9 +1,16 @@
 #include <string>
-#include "include/Balde.hpp"
 using namespace std;
 
-Balde::Balde(int m, int dlocal) {
-    this.tamanhoM = m;
-    this.dlocal = dlocal;
-    this.pseudochaves = new string[m];
+class Balde {
+    private: 
+        string* pseudochaves;
+        int tamanhoM;
+        int dlocal;
+    public: 
+        Balde(int m, int dlocal) {
+            this.tamanhoM = m;
+            this.dlocal = dlocal;
+            this.pseudochaves = new string[m];
+        }
+
 }
