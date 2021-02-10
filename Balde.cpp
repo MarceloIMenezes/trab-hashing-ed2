10d6499479c32f6634cf27cc0dc3f741c5746112
Marcelo Ian <marcelo.menezes@estudante.ufjf.br>
2021-02-10 10:46:28 -0300

    cria Balde inicial

---
+++ Balde.cpp
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+
+class Balde {
+    private: 
+        string* pseudochaves;
+        int tamanhoM;
+        int dlocal;
+    public: 
+        Balde(int m, int dlocal) {
+            this.tamanhoM = m;
+            this.dlocal = dlocal;
+            this.pseudochaves = new string[m];
+        }
+
+}
