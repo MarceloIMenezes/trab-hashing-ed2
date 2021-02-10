678a2cc2689b92964466ae3a87171577ee6d307c
Marcelo Ian <marcelo.menezes@estudante.ufjf.br>
2021-02-10 11:23:18 -0300

    cria um diretorio inicial

---
+++ Diretorio.cpp
@@ -0,0 +1,6 @@
+#include "./include/Balde.hpp"
+#include "./include/Diretorio.hpp"
+
+Diretorio::Diretorio(int balde_size, int dglobal) {
+    this->dglobal = dglobal;
+}
---
+++ include/Diretorio.hpp
@@ -0,0 +1,10 @@
+#include "Balde.hpp"
+
+class Diretorio {
+    private: 
+        Balde* balde;
+        int dglobal;
+        int tamanho;
+    public:
+        Diretorio(int balde_size, int dglobal);
+};
