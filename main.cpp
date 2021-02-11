1c86b25802c6da2bb654501d3a75dfaf393eb384
Marcelo Ian <marcelo.menezes@estudante.ufjf.br>
2021-02-11 15:03:08 -0300

    gera chaves aleatorias

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <random>
+#include <time.h>
 
 #include "./include/Diretorio.hpp"
 #include "./include/Balde.hpp"
 using namespace std;
+
+#define N 5
 
 
 int main (int argc, char *argv[]) {
@@ -13,6 +17,18 @@ int main (int argc, char *argv[]) {
     cin >> tamanhoBalde;
     cout << "Insira o número de bits a ser usado nas pseudo-chaves: ";
     cin >> dglobalMax;
-    
+
+    Diretorio* diretorio = new Diretorio(dglobalMax, tamanhoBalde);
+
+    for (int j=0; j<N; j++) {
+        string chave = "";
+        for(int i=0; i<dglobalMax; i++) {
+            srand(time(NULL));
+            int aux = rand() % 2;
+            chave += to_string(aux);
+        }
+        diretorio->insereChave(chave);
+    }
+
     return 0;
 }
